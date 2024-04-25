65ce16951302c4582f79358277a44dfbc9a65684
Devjyot <devjyotsnhsidhu@gmail.com>
2024-04-26 02:05:06 +0530

    Till custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,61 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Basic Flutter file
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GradientContainer extends StatelessWidget {
+  // const GradientContainer({key}): super(key: key); or
+  const GradientContainer({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.green,
+            Color.fromRGBO(100, 100, 100, 1),
           ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Color.fromRGBO(62, 20, 20, 1),
+            fontSize: 28,
+          ),
+        ), // Text is another widget class. the first param is positional and is the readable text.
+      ), // Center is used to center everything along both axis
+    ); // Basically div - cannot be const, nor can its ancestors
+  } // build s called when this widget is used
+} // Custom Widget; used for reusable widget blocks
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromRGBO(88, 14, 14, 1),
+        body: GradientContainer(),
+      ), // Scaffold is to organise and style elements
+    ), // MaterialApp() is a widget class and this syntax is the cnstructor declaration. This is the root element too, somewhat like <html>
+  ); // Calling the runApp() function from flutter/material.dart package; show the App UI
+} // Custom Function; has to be the main function to execute on app
+
+
+// Note: Dart supports both named as well as positional parameters.
+// Syntax
+// void add1(int num1, int num2) { // Positional Arguments
+// num1 + num2;
+// }
+
+// void add2({int num1, int num2}) { // Named Arguments
+// num1 + num2;
+// }
+
+// Calling Example:
+// add1(1, 2);
+// add2(num2: 1, num1: 2);
+
+// Named params are always defined after positional params
+
+// Named params can have definition like add2({int? num1, int num2 = 2}) // Implies that num1 is optional and that num2 has a default value of 2
